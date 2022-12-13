3622f7ffa95615ded0506239fa67e433a1986526
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2022-12-13 11:45:05 +0200

    Add files via upload

---
+++ 2.71.py
@@ -0,0 +1,9 @@
+#2.71
+timi=input("Dose tin timi polisis:")
+antitipa=int(input("Dose ta antitipa:"))
+aksia=timi*antitipa
+print aksia
+if aksia>150:
+    ekpt=aksia*9/100
+    nea_aksia=aksia-ekpt
+    print "AKSIA ME EKPTOSI"
