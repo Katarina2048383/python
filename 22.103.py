942771ab60acf6e22f8f2590316aa8cd5b338916
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2022-12-13 11:47:28 +0200

    Add files via upload
    2
    103

---
+++ 22.103.py
@@ -0,0 +1,10 @@
+#2.103
+megethos=raw_input("megethos royxwn:")
+if megethos=="S":
+    print"Small"
+elif megethos=="M":
+    print"Medium"
+elif megethos=="L":
+    print"Large"
+else:
+    print "mi epitrepomeno megethos"
