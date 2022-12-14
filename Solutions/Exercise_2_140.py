8decc0a945dcf37d4f5de2807be73cd3ca24d03e
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2022-12-14 08:21:33 +0100

    Create Exercise_2_140.py

---
+++ Solutions/Exercise_2_140.py
@@ -0,0 +1,11 @@
+# Exercise 2.140
+
+x=int(input("akeraio arithmo:"))
+      
+if x>0:
+    if x%2==0:
+       print"thetikos-artios"
+    else:
+       print"thetikos-peittos"
+else:
+    print"mi thetikos"
