4b3a3fbf528475d71e5a02d59783ce69993f274e
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2022-12-14 08:32:56 +0100

    Create Exercise_2_71.py

---
+++ Solutions/Exercise_2_71.py
@@ -0,0 +1,13 @@
+# Exercise 2.71
+
+timi=input("Dose tin timi polisis: ")
+antitipa=int(input("Dose ta antitipa: "))
+
+aksia=timi*antitipa
+
+print aksia
+
+if aksia>150:
+    ekpt=aksia*9/100.0
+    nea_aksia=aksia-ekpt
+    print "AKSIA ME EKPTOSI", nea_aksia
