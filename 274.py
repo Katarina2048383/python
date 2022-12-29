2b69af5f1d6cf273302589d8b14fa839f57be4ba
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2022-12-29 23:54:29 +0200

    Add files via upload

---
+++ 274.py
@@ -0,0 +1,9 @@
+#2.74
+x=input("dose tin proti thermokrasia:")
+y=input("dose tin deyteri hermokrasia:")
+v=input("dose tin triti thermokrasia:")
+z=(x+y+v)/3.0
+if z>25:
+    print"katalites gia xrisi"
+else:
+    print"akatalliles gia xrisi"
