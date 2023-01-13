7d8702219658252e1cae8e344fbdac974b2678f1
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2023-01-13 10:27:28 +0200

    Add files via upload

---
+++ 2.211.py
@@ -0,0 +1,32 @@
+#2.211
+
+#a.
+
+i=1
+while i <100:
+    i+=1
+    print i
+
+#b.
+
+i=2
+while i<200:
+    i+=2
+    print i*i
+
+
+#g.
+
+x=input()
+i=51
+while i>10:
+    i=i-3
+    print i*x
+
+#d.
+s=0
+i=-1
+while i>-10:
+    s=s+i
+    i=i-1
+    print s
