d31717cc9e9d2d570734e721a566af663fb35039
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2023-01-13 10:52:23 +0200

    Add files via upload

---
+++ 2.212.py
@@ -0,0 +1,32 @@
+#2.212
+
+#a.
+
+for i in range(2,100,2):
+    print i
+
+
+#b.
+
+P=1
+for I in range(1,50):
+    P=P*I
+    print I
+
+
+#g.
+
+S=0
+for I in range(20,0,-1):
+    X=input()
+   S=S+X
+   print S
+
+#d.
+
+S=0
+for I in range(200,-5):
+    S=S+pow(X,2)
+    print S
+
+    
