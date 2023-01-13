876cc923935fa90c7255cc951b92d1b69682d568
Katarina2048383 <120161173+Katarina2048383@users.noreply.github.com>
2023-01-13 08:58:30 +0200

    Add files via upload

---
+++ 2.56.py
@@ -0,0 +1,20 @@
+#2.56
+
+#a.
+A,B=input()
+if A<=B:
+    Z=4/100.0
+if A>B:
+    Z=7/100.0
+X=A+B*Z
+print X
+#B.
+A,B=input()
+if(A>0)or(B<A):
+    C=A-10
+    B=C/B
+if(A<=0)or(B>=A):
+   C=B-A
+   A=C/2
+print A,B,C
+    
